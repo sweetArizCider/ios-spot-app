d68b84111acbe4e85c19d3e2ea854f038ce95f09
pamelita123 <pame.ado3780@gmail.com>
2025-11-26 19:31:50 -0600

    avances

---
+++ ceport/ceport/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  ceport
+//
+//  Created by mac on 26/11/25.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var labelUserInput: UITextField!
+    
+    @IBOutlet weak var labelPasswordInput: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func buttonContinue(_ sender: Any) {
+    }
+    
+}
---
+++ ceport/ceport/ParkingViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ParkingViewController.swift
+//  ceport
+//
+//  Created by mac on 26/11/25.
+//
+
+import UIKit
+
+class ParkingViewController: UIViewController {
+
+    @IBOutlet weak var buttonSpot0: UIButton!
+    @IBOutlet weak var buttonSpot1: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
